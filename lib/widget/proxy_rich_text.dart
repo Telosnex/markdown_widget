24a0f1ee67a62ea760a524198adafb6c034fdfd7
James O'Leary <65884233+jpohhhh@users.noreply.github.com>
2024-12-03 15:44:58 -0500

    Fix web text selection x copy

--- lib/widget/proxy_rich_text.dart
+++ lib/widget/proxy_rich_text.dart
@@ -20,10 +20,27 @@ class ProxyRichText extends StatelessWidget {
       return richTextBuilder!.call(textSpan);
     }
 
-    if (kIsWeb) {
-      return Text.rich(textSpan);
-    } else {
-      return Text.rich(TextSpan(children: [textSpan, TextSpan(text: '\r')]));
-    }
+    // This ensures drag-select includes newlines for code blocks. Without
+    // injecting a newline, it is one contiguous block when copied. This
+    // is due to complex issues with the Flutter selection system. See
+    // https://github.com/flutter/flutter/issues/104548#issuecomment-2051481671
+    return Text.rich(
+      TextSpan(
+        children: [
+          textSpan,
+          // Native won't show \r.
+          // Web will show \r and \n.
+          // Use \n and set font size and height to 0 to make it invisible.
+          // Per https://github.com/flutter/flutter/issues/104548#issuecomment-2051481671
+          const TextSpan(
+            text: '\r',
+            style: TextStyle(
+              fontSize: 0,
+              height: 0,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
